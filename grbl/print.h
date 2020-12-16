3303b343d76ee7ff61fe56a3d0ec4b200240be16
Cc <1002667554@qq.com>
2020-12-16 12:04:51 +0800

    添加函数声明，避免warning

--- grbl/print.h
+++ grbl/print.h
@@ -43,8 +43,10 @@ void printFloatAll(float n, uint8_t decimal_places);
 //  - CoordValue: Handles all position or coordinate values in inches or mm reporting.
 //  - RateValue: Handles feed rate and current velocity in inches or mm reporting.
 void printFloat_CoordValue(float n);
-void printFloat_RateValue(float n);
+void printFloat_CoordValue_All(float n);
 
+void printFloat_RateValue(float n);
+void printFloat_RateValue_All(float n);
 // Debug tool to print free memory in bytes at the called point. Not used otherwise.
 void printFreeMemory();
 
