76f0f727a908bed30faba3d45d62e5de1f045cec
ChengJunJie <1002667554@qq.com>
2020-12-24 16:01:19 +0800

    修改中断优先级分组，steper中断优先原则

--- stm32/main.c
+++ stm32/main.c
@@ -206,11 +206,12 @@ void SystemClock_Config(void)
   */
 static void MX_NVIC_Init(void)
 {
+  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_2);
   /* EXTI0_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(EXTI0_IRQn, 2, 2);
   HAL_NVIC_EnableIRQ(EXTI0_IRQn);
   /* EXTI1_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(EXTI1_IRQn, 1, 1);
   HAL_NVIC_EnableIRQ(EXTI1_IRQn);
 }
 
