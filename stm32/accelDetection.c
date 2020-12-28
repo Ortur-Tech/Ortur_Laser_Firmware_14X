36c9704eebdf77e4305db7ff42db8d4f54fe059d
ChengJunJie <1002667554@qq.com>
2020-12-28 19:06:30 +0800

    Revert "格式化打印提示"
    This reverts commit 97e374004448016184a4129a6c5ea144a0590b2d.

--- stm32/accelDetection.c
+++ stm32/accelDetection.c
@@ -100,12 +100,72 @@ uint8_t GsensorDeviceType=0;    //!<gsensor芯片类型
  */
 void BMA250_Init(void)
 {
-	Write_One_Byte_iicaddr(BMA250_Addr, 0x17, 0b111);//使能xyz加速度过高中断
-	Write_One_Byte_iicaddr(BMA250_Addr, 0x19, 0b10);//中断配置到int1脚
-	Write_One_Byte_iicaddr(BMA250_Addr, 0x25, 0x05);//过高时间10ms
-	Write_One_Byte_iicaddr(BMA250_Addr, 0x26, 130);//过高阈值
+    uint8_t bGRange = 0;                                  // g Range;
+    uint8_t bBwd = 0;                                     // Bandwidth
+    uint8_t bSleep = 0;                                   // Sleep phase
 
-	delay_ms(5);
+// Configure sensor and start to sample data
+#if (BMP_AS_RANGE == 2)
+	bGRange = 0x03;
+#elif (BMP_AS_RANGE == 4)
+	bGRange = 0x05;
+#elif (BMP_AS_RANGE == 8)
+	bGRange = 0x08;
+#elif (BMP_AS_RANGE == 16)
+	bGRange = 0x0C;
+#else
+//	#error "Measurement range not supported"
+#endif
+
+#if (BMP_AS_BANDWIDTH == 8)
+	bBwd = 0x08;
+#elif (BMP_AS_BANDWIDTH == 16)
+	bBwd = 0x09;
+#elif (BMP_AS_BANDWIDTH == 31)
+	bBwd = 0x0A;
+#elif (BMP_AS_BANDWIDTH == 63)
+	bBwd = 0x0B;
+#elif (BMP_AS_BANDWIDTH == 125)
+	bBwd = 0x0C;
+#elif (BMP_AS_BANDWIDTH == 250)
+	bBwd = 0x0D;
+#elif (BMP_AS_BANDWIDTH == 500)
+	bBwd = 0x0E;
+#elif (BMP_AS_BANDWIDTH == 1000)
+	bBwd = 0x0F;
+#else
+//	#error "Sample rate not supported"
+#endif
+
+#if (BMP_AS_SLEEPPHASE == 1)
+	bSleep = 0x4C;
+#elif (BMP_AS_SLEEPPHASE == 2)
+	bSleep = 0x4E;
+#elif (BMP_AS_SLEEPPHASE == 4)
+	bSleep = 0x50;
+#elif (BMP_AS_SLEEPPHASE == 6)
+	bSleep = 0x52;
+#elif (BMP_AS_SLEEPPHASE == 10)
+	bSleep = 0x54;
+#elif (BMP_AS_SLEEPPHASE == 25)
+	bSleep = 0x56;
+#elif (BMP_AS_SLEEPPHASE == 50)
+	bSleep = 0x58;
+#else
+//	#error "Sleep phase duration not supported"
+#endif
+
+
+	// write sensor configuration
+	Write_One_Byte_iicaddr(BMA250_Addr, BMP_GRANGE, bGRange);  // Set measurement range
+	Write_One_Byte_iicaddr(BMA250_Addr, BMP_BWD, bBwd);        // Set filter bandwidth
+	Write_One_Byte_iicaddr(BMA250_Addr, BMP_PM, bSleep);       // Set filter bandwidth
+
+
+#ifndef BMP_AS_FILTERING
+	Write_One_Byte_iicaddr(BMA250_Addr, BMP_SCR, 0x80);        // acquire unfiltered acceleration data
+#endif
+
 }
 /**
  * @brief Gsensor_Init
